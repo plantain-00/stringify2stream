2ab1172d9f20702bb6400b91b37510463b5874b8
york yao <yaoao12306@outlook.com>
2018-01-20 10:57:44 +0800

    refactor: use tslint-config-standard as lint rule

--- spec/indexSpec.ts
+++ spec/indexSpec.ts
@@ -1,137 +1,136 @@
-import * as stream from "stream";
-import stringify from "../dist/nodejs";
+import * as stream from 'stream'
+import stringify from '../dist/nodejs'
 
-function test(value: any, replacer?: any, space?: string | number) {
-    let result: string | undefined = "";
-    const writeStream = new stream.Writable({
-        write(chunk, encoding, callback) {
-            result += chunk.toString();
-            callback();
-        },
-    });
-    stringify(value, data => {
-        if (data === undefined) {
-            result = undefined;
-        } else {
-            writeStream.write(data);
-        }
-    }, replacer, space);
-    writeStream.end();
-    expect(result).toEqual(JSON.stringify(value, replacer, space));
+function test (value: any, replacer?: any, space?: string | number) {
+  let result: string | undefined = ''
+  const writeStream = new stream.Writable({
+    write (chunk, encoding, callback) {
+      result += chunk.toString()
+      callback()
+    }
+  })
+  stringify(value, data => {
+    if (data === undefined) {
+      result = undefined
+    } else {
+      writeStream.write(data)
+    }
+  }, replacer, space)
+  writeStream.end()
+  expect(result).toEqual(JSON.stringify(value, replacer, space))
 }
 
 // tslint:disable:no-construct
-// tslint:disable:only-arrow-functions
 
-it("", () => {
-    test(undefined);
-    test({});
-    test(true);
-    test(new Boolean(true));
-    test("foo");
-    test(1);
-    test(new Number(1));
-    test(() => 1);
-    test(new String("foo"));
-    test(Symbol("foo"));
-    test([1, "false", false]);
-    test({ x: 5 });
-    test(new Date(2006, 0, 2, 15, 4, 5));
-    test({ x: 5, y: 6 });
-    test([new Number(3), new String("false"), new Boolean(false)]);
-    test({ x: [10, undefined, () => { /**/ }, Symbol("")] });
-    test({ x: undefined, y: Object, z: Symbol("") });
-    test({ [Symbol("foo")]: "foo" });
-    test(Object.create(null, { x: { value: "x", enumerable: false }, y: { value: "y", enumerable: true } }));
-    test({ a: 2 }, null, " ");
-    test({ a: 2 }, null, "  ");
-    test({ a: 2 }, null, 3);
-    test({ a: 2, b: "abc" }, null, " ");
-    test([1, 2, 3], null, " ");
-    test([]);
-    test([], null, 1);
-    test({});
-    test({}, null, 1);
-    test({ a: [1, 2, 3] }, null, " ");
-    test([{ a: 1, b: [2] }], null, " ");
-    test({ uno: 1, dos: 2 }, null, "\t");
-    test({
-        foo: "foo",
-        bar() {
-            return "bar";
-        },
-    });
-    const obj = {
-        foo: "foo",
-        toJSON() {
-            return "bar";
-        },
-    };
-    test(obj);
-    test({ x: obj });
-    const obj2 = {
-        foo: "foo",
-        toJSON(key: string) {
-            if (key === "") {
-                return "bar only";
-            } else {
-                return "bar in " + key;
-            }
-        },
-    };
-    test(obj2);
-    test({ x: obj2 });
-    test([obj2, obj2]);
-    test({ foo: "a\"b" });
-    test({
-        name: "stringify2stream",
-        version: "1.0.0",
-        description: "A js library to stringify json to stream to avoid out-of-memory of JSON.stringify.",
-        main: "index.js",
-        scripts: {
-            build: "clean-scripts build",
-            lint: "clean-scripts lint",
-            test: "clean-scripts test",
-            fix: "clean-scripts fix",
-            release: "clean-scripts release",
-        },
-        repository: {
-            type: "git",
-            url: "git+https://github.com/plantain-00/stringify2stream.git",
-        },
-        author: "York Yao",
-        license: "MIT",
-        bugs: {
-            url: "https://github.com/plantain-00/stringify2stream/issues",
-        },
-        homepage: "https://github.com/plantain-00/stringify2stream#readme",
-        devDependencies: {
-            "@types/jasmine": "2.6.0",
-            "clean-release": "1.3.5",
-            "clean-scripts": "1.2.6",
-            "jasmine": "2.8.0",
-            "no-unused-export": "1.2.7",
-            "rimraf": "2.6.2",
-            "rollup": "0.50.0",
-            "rollup-plugin-node-resolve": "3.0.0",
-            "rollup-plugin-uglify": "2.0.1",
-            "standard": "10.0.3",
-            "tslint": "5.7.0",
-            "typescript": "2.5.2",
-        },
-    }, null, "  ");
-    test({ a: 2 }, null, 11);
-    test({ a: 2 }, null, 0);
-    test({ a: 2 }, null, 2.9);
-    test({ a: 2 }, null, NaN);
-    test({ a: 2 }, null, "1234567890abc");
-    const replacer = (key: string, value: any) => {
-        if (typeof value === "string") {
-            return undefined;
-        }
-        return value;
-    };
-    test({ foundation: "Mozilla", model: "box", week: 45, transport: "car", month: 7 }, replacer);
-    test(["foo", 123], replacer);
-    test({ foundation: "Mozilla", model: "box", week: 45, transport: "car", month: 7 }, ["week", "month"]);
-});
+it('', () => {
+  test(undefined)
+  test({})
+  test(true)
+  test(new Boolean(true))
+  test('foo')
+  test(1)
+  test(new Number(1))
+  test(() => 1)
+  test(new String('foo'))
+  test(Symbol('foo'))
+  test([1, 'false', false])
+  test({ x: 5 })
+  test(new Date(2006, 0, 2, 15, 4, 5))
+  test({ x: 5, y: 6 })
+  test([new Number(3), new String('false'), new Boolean(false)])
+  test({ x: [10, undefined, () => { /**/ }, Symbol('')] })
+  test({ x: undefined, y: Object, z: Symbol('') })
+  test({ [Symbol('foo')]: 'foo' })
+  test(Object.create(null, { x: { value: 'x', enumerable: false }, y: { value: 'y', enumerable: true } }))
+  test({ a: 2 }, null, ' ')
+  test({ a: 2 }, null, '  ')
+  test({ a: 2 }, null, 3)
+  test({ a: 2, b: 'abc' }, null, ' ')
+  test([1, 2, 3], null, ' ')
+  test([])
+  test([], null, 1)
+  test({})
+  test({}, null, 1)
+  test({ a: [1, 2, 3] }, null, ' ')
+  test([{ a: 1, b: [2] }], null, ' ')
+  test({ uno: 1, dos: 2 }, null, '\t')
+  test({
+    foo: 'foo',
+    bar () {
+      return 'bar'
+    }
+  })
+  const obj = {
+    foo: 'foo',
+    toJSON () {
+      return 'bar'
+    }
+  }
+  test(obj)
+  test({ x: obj })
+  const obj2 = {
+    foo: 'foo',
+    toJSON (key: string) {
+      if (key === '') {
+        return 'bar only'
+      } else {
+        return 'bar in ' + key
+      }
+    }
+  }
+  test(obj2)
+  test({ x: obj2 })
+  test([obj2, obj2])
+  test({ foo: 'a"b' })
+  test({
+    name: 'stringify2stream',
+    version: '1.0.0',
+    description: 'A js library to stringify json to stream to avoid out-of-memory of JSON.stringify.',
+    main: 'index.js',
+    scripts: {
+      build: 'clean-scripts build',
+      lint: 'clean-scripts lint',
+      test: 'clean-scripts test',
+      fix: 'clean-scripts fix',
+      release: 'clean-scripts release'
+    },
+    repository: {
+      type: 'git',
+      url: 'git+https://github.com/plantain-00/stringify2stream.git'
+    },
+    author: 'York Yao',
+    license: 'MIT',
+    bugs: {
+      url: 'https://github.com/plantain-00/stringify2stream/issues'
+    },
+    homepage: 'https://github.com/plantain-00/stringify2stream#readme',
+    devDependencies: {
+      '@types/jasmine': '2.6.0',
+      'clean-release': '1.3.5',
+      'clean-scripts': '1.2.6',
+      'jasmine': '2.8.0',
+      'no-unused-export': '1.2.7',
+      'rimraf': '2.6.2',
+      'rollup': '0.50.0',
+      'rollup-plugin-node-resolve': '3.0.0',
+      'rollup-plugin-uglify': '2.0.1',
+      'standard': '10.0.3',
+      'tslint': '5.7.0',
+      'typescript': '2.5.2'
+    }
+  }, null, '  ')
+  test({ a: 2 }, null, 11)
+  test({ a: 2 }, null, 0)
+  test({ a: 2 }, null, 2.9)
+  test({ a: 2 }, null, NaN)
+  test({ a: 2 }, null, '1234567890abc')
+  const replacer = (key: string, value: any) => {
+    if (typeof value === 'string') {
+      return undefined
+    }
+    return value
+  }
+  test({ foundation: 'Mozilla', model: 'box', week: 45, transport: 'car', month: 7 }, replacer)
+  test(['foo', 123], replacer)
+  test({ foundation: 'Mozilla', model: 'box', week: 45, transport: 'car', month: 7 }, ['week', 'month'])
+})
--- src/index.ts
+++ src/index.ts
@@ -1,178 +1,178 @@
 /**
  * @public
  */
-export default function stringify(value: any, write: (data: string | undefined) => void, replacer?: ((key: string, value: any) => any) | (number | string)[] | null, space?: string | number) {
-    if (isInvalidValue(value)) {
-        write(undefined);
-        return;
-    }
-    let indent: string | undefined;
-    if (typeof space === "number"
+export default function stringify (value: any, write: (data: string | undefined) => void, replacer?: ((key: string, value: any) => any) | (number | string)[] | null, space?: string | number) {
+  if (isInvalidValue(value)) {
+    write(undefined)
+    return
+  }
+  let indent: string | undefined
+  if (typeof space === 'number'
         && !isNaN(space)
         && space >= 1) {
-        indent = "";
-        const spaceCount = Math.min(Math.floor(space), 10);
-        for (let i = 0; i < spaceCount; i++) {
-            indent += " ";
-        }
-    } else if (typeof space === "string") {
-        indent = space.substring(0, 10);
+    indent = ''
+    const spaceCount = Math.min(Math.floor(space), 10)
+    for (let i = 0; i < spaceCount; i++) {
+      indent += ' '
     }
+  } else if (typeof space === 'string') {
+    indent = space.substring(0, 10)
+  }
 
-    stringifyInternally(value, write, replacer, indent, "", { type: ParentType.root });
+  stringifyInternally(value, write, replacer, indent, '', { type: ParentType.root })
 }
 
 const enum ParentType {
     root,
     array,
-    object,
+    object
 }
 
 type Parent =
     {
-        type: ParentType.root;
+      type: ParentType.root;
     }
     | {
-        type: ParentType.array;
-        index: number;
+      type: ParentType.array;
+      index: number;
     } | {
-        type: ParentType.object;
-        propertyName: string;
-    };
-
-function stringifyInternally(value: any, write: (data: string) => void, replacer: ((key: string, value: any) => any) | (number | string)[] | null | undefined, baseIndent: string | undefined, indent: string, parent: Parent) {
-    const currentIndent = baseIndent === undefined ? indent : indent + baseIndent;
-    if (Array.isArray(value)) {
-        write(`[`);
-        for (let i = 0; i < value.length; i++) {
-            if (baseIndent !== undefined) {
-                write(`\n${currentIndent}`);
-            }
-            if (isInvalidValue(value[i])) {
-                write(`null`);
-            } else {
-                let child = value[i];
-                if (replacer) {
-                    if (typeof replacer === "function") {
-                        const result = replacer(i.toString(), value[i]);
-                        if (isInvalidValue(result)) {
-                            write(`null`);
-                            if (i !== value.length - 1) {
-                                write(`,`);
-                            }
-                            continue;
-                        } else {
-                            child = result;
-                        }
-                    }
-                }
-
-                stringifyInternally(child, write, replacer, baseIndent, currentIndent, {
-                    type: ParentType.array,
-                    index: i,
-                });
-            }
-            if (i !== value.length - 1) {
-                write(`,`);
-            }
-        }
-        if (baseIndent !== undefined && value.length > 0) {
-            write(`\n${indent}]`);
-        } else {
-            write(`]`);
-        }
-    } else if (typeof value === "string" || value instanceof String) {
-        write(escapeQuote(value.toString()));
-    } else if (typeof value === "boolean"
-        || value instanceof Boolean
-        || typeof value === "number"
-        || value instanceof Number) {
-        write(`${value.toString()}`);
-    } else if (value === null) {
-        write(`null`);
-    } else if (value instanceof Date) {
-        write(escapeQuote(value.toISOString()));
-    } else if (typeof value === "object") {
-        if (typeof value.toJSON === "function") {
-            if (parent.type === ParentType.root) {
-                write(escapeQuote(value.toJSON("")));
-            } else if (parent.type === ParentType.array) {
-                write(escapeQuote(value.toJSON(parent.index.toString())));
-            } else {
-                write(escapeQuote(value.toJSON(parent.propertyName)));
-            }
-            return;
-        }
-
-        write(`{`);
-        let canEmitComma = false;
-        // tslint:disable-next-line:forin
-        for (const key in value) {
-            let child = value[key];
-            if (isInvalidValue(child)) {
-                continue;
-            }
-
-            if (replacer) {
-                if (typeof replacer === "function") {
-                    const result = replacer(key, child);
-                    if (isInvalidValue(result)) {
-                        continue;
-                    } else {
-                        child = result;
-                    }
-                } else if (Array.isArray(replacer)) {
-                    if (replacer.every(r => r !== key)) {
-                        continue;
-                    }
-                }
-            }
-
-            if (canEmitComma) {
-                write(`,`);
-            }
-            if (baseIndent !== undefined) {
-                write(`\n${currentIndent}${escapeQuote(key)}: `);
-            } else {
-                write(`${escapeQuote(key)}:`);
-            }
-            canEmitComma = true;
-            stringifyInternally(child, write, replacer, baseIndent, currentIndent, {
-                type: ParentType.object,
-                propertyName: key,
-            });
-        }
-        if (baseIndent !== undefined && Object.keys(value).length > 0) {
-            write(`\n${indent}}`);
-        } else {
-            write(`}`);
-        }
+      type: ParentType.object;
+      propertyName: string;
     }
+
+function stringifyInternally (value: any, write: (data: string) => void, replacer: ((key: string, value: any) => any) | (number | string)[] | null | undefined, baseIndent: string | undefined, indent: string, parent: Parent) {
+  const currentIndent = baseIndent === undefined ? indent : indent + baseIndent
+  if (Array.isArray(value)) {
+    write(`[`)
+    for (let i = 0; i < value.length; i++) {
+      if (baseIndent !== undefined) {
+        write(`\n${currentIndent}`)
+      }
+      if (isInvalidValue(value[i])) {
+        write(`null`)
+      } else {
+        let child = value[i]
+        if (replacer) {
+          if (typeof replacer === 'function') {
+            const result = replacer(i.toString(), value[i])
+            if (isInvalidValue(result)) {
+              write(`null`)
+              if (i !== value.length - 1) {
+                write(`,`)
+              }
+              continue
+            } else {
+              child = result
+            }
+          }
+        }
+
+        stringifyInternally(child, write, replacer, baseIndent, currentIndent, {
+          type: ParentType.array,
+          index: i
+        })
+      }
+      if (i !== value.length - 1) {
+        write(`,`)
+      }
+    }
+    if (baseIndent !== undefined && value.length > 0) {
+      write(`\n${indent}]`)
+    } else {
+      write(`]`)
+    }
+  } else if (typeof value === 'string' || value instanceof String) {
+    write(escapeQuote(value.toString()))
+  } else if (typeof value === 'boolean'
+        || value instanceof Boolean
+        || typeof value === 'number'
+        || value instanceof Number) {
+    write(`${value.toString()}`)
+  } else if (value === null) {
+    write(`null`)
+  } else if (value instanceof Date) {
+    write(escapeQuote(value.toISOString()))
+  } else if (typeof value === 'object') {
+    if (typeof value.toJSON === 'function') {
+      if (parent.type === ParentType.root) {
+        write(escapeQuote(value.toJSON('')))
+      } else if (parent.type === ParentType.array) {
+        write(escapeQuote(value.toJSON(parent.index.toString())))
+      } else {
+        write(escapeQuote(value.toJSON(parent.propertyName)))
+      }
+      return
+    }
+
+    write(`{`)
+    let canEmitComma = false
+        // tslint:disable-next-line:forin
+    for (const key in value) {
+      let child = value[key]
+      if (isInvalidValue(child)) {
+        continue
+      }
+
+      if (replacer) {
+        if (typeof replacer === 'function') {
+          const result = replacer(key, child)
+          if (isInvalidValue(result)) {
+            continue
+          } else {
+            child = result
+          }
+        } else if (Array.isArray(replacer)) {
+          if (replacer.every(r => r !== key)) {
+            continue
+          }
+        }
+      }
+
+      if (canEmitComma) {
+        write(`,`)
+      }
+      if (baseIndent !== undefined) {
+        write(`\n${currentIndent}${escapeQuote(key)}: `)
+      } else {
+        write(`${escapeQuote(key)}:`)
+      }
+      canEmitComma = true
+      stringifyInternally(child, write, replacer, baseIndent, currentIndent, {
+        type: ParentType.object,
+        propertyName: key
+      })
+    }
+    if (baseIndent !== undefined && Object.keys(value).length > 0) {
+      write(`\n${indent}}`)
+    } else {
+      write(`}`)
+    }
+  }
 }
 
-function isInvalidValue(value: any) {
-    return value === undefined
-        || typeof value === "function"
-        || typeof value === "symbol";
+function isInvalidValue (value: any) {
+  return value === undefined
+        || typeof value === 'function'
+        || typeof value === 'symbol'
 }
 
-const quoteEscapeRegExp = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+const quoteEscapeRegExp = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g
 const meta: { [key: string]: string | undefined } = {
-    "\b": "\\b",
-    "\t": "\\t",
-    "\n": "\\n",
-    "\f": "\\f",
-    "\r": "\\r",
-    "\"": "\\\"",
-    "\\": "\\\\",
-};
+  '\b': '\\b',
+  '\t': '\\t',
+  '\n': '\\n',
+  '\f': '\\f',
+  '\r': '\\r',
+  '"': '\\"',
+  '\\': '\\\\'
+}
 
-function escapeQuote(str: string) {
-    quoteEscapeRegExp.lastIndex = 0;
-    return quoteEscapeRegExp.test(str)
-        ? "\"" + str.replace(quoteEscapeRegExp, a => {
-            const c = meta[a];
-            return typeof c === "string" ? c : "\\u" + ("0000" + a.charCodeAt(0).toString(16)).slice(-4);
-        }) + "\""
-        : "\"" + str + "\"";
+function escapeQuote (str: string) {
+  quoteEscapeRegExp.lastIndex = 0
+  return quoteEscapeRegExp.test(str)
+        ? '"' + str.replace(quoteEscapeRegExp, a => {
+          const c = meta[a]
+          return typeof c === 'string' ? c : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4)
+        }) + '"'
+        : '"' + str + '"'
 }
