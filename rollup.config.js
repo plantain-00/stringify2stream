20deaa3297622d27492c8b133e3d302ca9b4d210
york yao <yaoao12306@outlook.com>
2018-10-30 22:51:51 +0800

    chore: add type-coverage check

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 export default {
   input: 'dist/browser/index.js',
   name: 'Stringify2stream',
-  plugins: [resolve(), uglify()],
+  plugins: [resolve({ browser: true }), uglify()],
   output: {
     file: 'dist/stringify2stream.min.js',
     format: 'umd'
