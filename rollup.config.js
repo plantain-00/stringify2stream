bbe8fdb16e3fdf453db198ee64d15dd84499e12e
york yao <yaoao12306@outlook.com>
2018-12-30 08:41:26 +0800

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'dist/browser/index.js',
-  name: 'Stringify2stream',
   plugins: [resolve({ browser: true }), uglify()],
   output: {
+    name: 'Stringify2stream',
     file: 'dist/stringify2stream.min.js',
     format: 'umd'
   }
